90518dbe6a9cb8d4bab539991a1054262fe04023
CaloMorell <oscarmorell07@yahoo.com>
2024-01-05 09:34:50 -0400

    2. Basic annotations - floor

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+
+"""
+    Esta función toma un número flotante como entrada y devuelve su valor entero truncado (piso).
+"""
+
+def floor(n: float) -> int:
+    """ Devuelve el valor entero truncado de un número flotante. """
+    return int(n)
