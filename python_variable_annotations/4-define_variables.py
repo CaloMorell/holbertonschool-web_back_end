54ad9e95654db92e86198de0c42af0b78e63b7f9
CaloMorell <oscarmorell07@yahoo.com>
2024-01-05 09:48:13 -0400

    4. Define variables

---
+++ python_variable_annotations/4-define_variables.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+Each variable is declared with its corresponding type and is assigned the specified value.
+"""
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
