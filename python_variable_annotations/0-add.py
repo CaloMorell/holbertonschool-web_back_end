7ad04b0ae26bad4f71e5d8c357719a2957137f16
CaloMorell <oscarmorell07@yahoo.com>
2024-01-05 08:55:31 -0400

    Basic annotations - add

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+"""Esta función toma dos números flotantes como entrada y devuelve su suma."""
+
+def add(a: float, b: float) -> float:
+    """Suma dos números."""
+    return a + b
