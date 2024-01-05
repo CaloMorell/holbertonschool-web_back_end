23cc8efcaaaa379df5263620ec290b8b0462362b
CaloMorell <oscarmorell07@yahoo.com>
2024-01-05 09:28:33 -0400

    1. Basic annotations - concat

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+"""
+Esta función toma dos cadenas como entrada y devuelve su concatenación.
+"""
+
+def concat(str1: str, str2: str) -> str:
+    """Concatena dos cadenas."""
+    return (str1 + str2)
