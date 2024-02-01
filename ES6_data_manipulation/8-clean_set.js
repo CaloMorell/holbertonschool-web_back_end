a4979fdff8c3e53a7be042f0968bd7c49095773b
CaloMorell <oscarmorell07@yahoo.com>
2024-02-01 08:00:15 -0400

    8. Clean set

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,8 +1,8 @@
 export default function cleanSet(set, startString) {
-let result = '';
-if (!startString || !startString.length) return result;
-set.forEach((i) => {
+  let result = '';
+  if (!startString || !startString.length) return result;
+  set.forEach((i) => {
     if (i && i.startsWith(startString)) result += `${i.slice(startString.length)}-`;
-});
-return result.slice(0, result.length - 1);
+  });
+  return result.slice(0, result.length - 1);
 }
